7fa7cfeb9791cd588a7e66f9e14cd2be9ffd4c31
Dovydas <ddorofejevas@yahoo.com>
2025-11-27 23:12:35 +0200

    switching to RealmSwift, commit before project settings update.

---
+++ Todoey/Model/Data.swift
@@ -0,0 +1,16 @@
+//
+//  Data.swift
+//  Todoey
+//
+//  Created by Dovydas Dorofejevas on 27/11/2025.
+//  Copyright © 2025 App Brewery. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Data: Object {
+    
+    @objc dynamic var text: String = ""
+    
+}
